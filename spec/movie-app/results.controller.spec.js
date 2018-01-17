1736b5c88e0b80252638ebe9490937b8d128e754
Jianwei Sun <sun.381@osu.edu>
2018-01-16 20:17:24 -0600

    update tests

--- spec/movie-app/results.controller.spec.js
+++ spec/movie-app/results.controller.spec.js
@@ -29,15 +29,28 @@ describe('Results Controller', function() {
 	var $rootScope;
 	var $scope;
 	var omdbApi;
+	var $exceptionHandler;
+	var $log;
 	
 	beforeEach(module('movieApp'));
 
-	beforeEach(inject(function(_$controller_, _$location_, _$q_, _$rootScope_, _omdbApi_) {
+	beforeEach(module(function($exceptionHandlerProvider) {
+	    $exceptionHandlerProvider.mode('log');
+	}));
+
+	beforeEach(module(function($logProvider) {
+		// We can configure the debugging level (the default is true)
+		$logProvider.debugEnabled(true);
+	}));
+
+	beforeEach(inject(function(_$controller_, _$location_, _$q_, _$rootScope_, _$exceptionHandler_, _$log_, _omdbApi_) {
 		$controller = _$controller_;
 		$scope = {};		
 		$location = _$location_;
 		$q = _$q_;
 		$rootScope = _$rootScope_;
+		$exceptionHandler = _$exceptionHandler_;
+		$log = _$log_;
 		omdbApi = _omdbApi_;
 	}));
 
@@ -54,17 +67,21 @@ describe('Results Controller', function() {
 		expect($scope.results[1].Title).toBe(results.Search[1].Title);
 		expect($scope.results[2].Title).toBe(results.Search[2].Title);
 		expect(omdbApi.search).toHaveBeenCalledWith('star wars');
+		//console.log($log.debug.logs[0]);
+		//expect($log.debug.logs[0]).toEqual(['Controller loaded with query: ', 'star wars']);
+		expect($log.debug.logs[0]).toEqual(['Data returned for query: ', 'star wars', results]);
 	});
 
 	it('should set result status to error', function() {
 		spyOn(omdbApi, 'search').and.callFake(function() {
 			var deferred = $q.defer();
-			deferred.reject();
+			deferred.reject('Something went wrong!');
 			return deferred.promise;
 		});
 		$location.search('q', 'star wars');
 		$controller('ResultsController', { $scope: $scope });
 		$rootScope.$apply();
-		expect($scope.errorMessage).toBe('Something went wrong!');
+		//console.log($exceptionHandler.errors);
+		expect($exceptionHandler.errors[0]).toEqual('Something went wrong!');
 	});
 });
--- src/movie-app/results.controller.js
+++ src/movie-app/results.controller.js
@@ -1,11 +1,20 @@
 angular.module('movieApp')
-	.controller('ResultsController', function($scope, $location, omdbApi) {
+	.controller('ResultsController', function($scope, $location, $exceptionHandler,  $log,omdbApi) {
 		var query = $location.search().q;
 		omdbApi.search(query)
 			.then(function(data) {
+				$log.debug('Data returned for query: ', query, data);
 				$scope.results = data.Search;
 			})
-			.catch(function() {
-				$scope.errorMessage = 'Something went wrong!'
+			.catch(function(e) {
+				$exceptionHandler(e);
 			});
+
+		$scope.expand = function expand(index, id) {
+			omdbApi.find(id)
+				.then(function(data) {
+					$scope.results[index].data = data;
+					$scope.results[index].open = true;
+				});
+		};
 	});
